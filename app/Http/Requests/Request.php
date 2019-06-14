c0630c59b36283202c82511dbce2e8b42bda45a0
Dustin蕭 <dustinhsiao21@gmail.com>
2019-06-14 17:03:05 +0800

    add basic

---
+++ app/Http/Requests/Request.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+abstract class Request extends FormRequest
+{
+    public function onlyRules()
+    {
+        $fields = array_keys($this->rules());
+        $fields = array_map(function ($field) {
+            return strstr($field, '.*', true) ?: $field;
+        }, $fields);
+        return $this->only($fields);
+    }
+
+    public function rules()
+    {
+        return [];
+    }
+}
+
---
+++ app/Repositories/BaseRepository.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Repositories;
+
+use Illuminate\Database\Eloquent\Model;
+
+abstract class BaseRepository 
+{
+    private $model;
+
+    public function __construct(Model $model)
+    {
+        $this->model = $model;
+    }
+
+    public function find($id)
+    {
+        return $this->model->find($id);
+    }
+
+    public function all()
+    {
+        return $this->model->all();
+    }
+
+    public function store(array $array)
+    {
+        return $this->model->create($array);
+    }
+}
