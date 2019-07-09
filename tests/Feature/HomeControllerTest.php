ba11eb2e9823e23c805c14e0ef3ced69596d9359
Dustin Hsiao <dustinhsiao21@gmail.com>
2019-07-09 09:41:36 +0000

    Apply fixes from StyleCI

--- tests/Feature/HomeControllerTest.php
+++ tests/Feature/HomeControllerTest.php
@@ -8,14 +8,14 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 class HomeControllerTest extends TestCase
 {
     use RefreshDatabase;
-    
+
     public function testHomePage()
     {
         $this->get(route('home'))->assertStatus(200)
             ->assertDontSee('Go to My Projects');
 
         $this->signIn();
-        
+
         $this->get(route('home'))->assertStatus(200)
             ->assertSee('Go to My Projects');
     }
