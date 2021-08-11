b9abd233f6f77da2d61989540870b591380dfdd1
Dustin Hsiao <dustinhsiao21@users.noreply.github.com>
2021-08-11 02:49:40 +0000

    Apply fixes from StyleCI

--- database/factories/TaskFactory.php
+++ database/factories/TaskFactory.php
@@ -2,8 +2,8 @@
 
 /* @var $factory \Illuminate\Database\Eloquent\Factory */
 
-use App\Models\Task;
 use App\Models\Project;
+use App\Models\Task;
 use Faker\Generator as Faker;
 
 $factory->define(Task::class, function (Faker $faker) {
